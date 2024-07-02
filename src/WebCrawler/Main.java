368f65885199e1aeee2a8c6792f1d81f20db2af2
Jeferson Souza <jeferson.2017317264@gmail.com>
2024-07-02 17:36:00 +0000

    change website

--- src/WebCrawler/Main.java
+++ src/WebCrawler/Main.java
@@ -3,7 +3,7 @@ package WebCrawler;
 public class Main {
   public static void main(String[] args) {
     WebCrawler crawler = new WebCrawler();
-    String rootURL = "https://www.unoesc.edu.br";
+    String rootURL = "https://beagro.com.br/";
     crawler.discoverWeb(rootURL);
   }
 }
